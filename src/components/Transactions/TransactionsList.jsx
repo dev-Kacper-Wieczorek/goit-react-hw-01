11b23771a3913744e25d3136f6bcb7a46c2ea16e
Kacpero60 <kapiw2002@gmail.com>
2024-12-10 16:38:49 +0100

    Update TransactionsList.jsx

--- src/components/Transactions/TransactionsList.jsx
+++ src/components/Transactions/TransactionsList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './components/Transactions/Transactions.module.css';
+import styles from './Transactions.module.css';
+
 
 const TransactionsList = ({ items }) => {
   return (
